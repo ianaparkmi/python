1e2eea9c353d69aa1d8af772a4f02ffd08422d35
ianaparkmi <ianaparkmi@gmail.com>
2025-09-19 18:34:16 +0300

    new

---
+++ task_1.py
@@ -0,0 +1,5 @@
+surname=input("Введите фамилию:")
+name=input("Введите имя:")
+patronymic=input("Введите отчество:")
+result=surname+" "+name[0]+"."+patronymic[0]+"."
+print("ФИО:",result)
---
+++ task_7.py
@@ -0,0 +1,4 @@
+second=int(input("enter a seconds"))
+hour=second//60
+second=second%60
+print(hour, 'hour ', second,' second')
