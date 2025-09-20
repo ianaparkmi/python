ad9cc7a9993af483204945b05a9cb4a8d95cac4a
ianaparkmi <ianaparkmi@gmail.com>
2025-09-20 17:37:34 +0300

    new

---
+++ task_10.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number. a= "))
+b=int(input('enter a number. b= '))
+sum=a+b
+dif=a-b
+multip=a*b
+quot=a/b
+remains=a%b
+degree=a**b
+print("sum: ",sum,
+      "difference: ",dif,
+      "multiplication: ", multip,
+      "the quotient of a and b", quot,
+      "the remainder of the division of a and b", remains,
+      "raising the number a to the degree of b", degree)
