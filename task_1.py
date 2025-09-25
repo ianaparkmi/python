c76f8f866e9046b4faa8ce53faf2c5ebfcdf6d43
ianaparkmi <ianaparkmi@gmail.com>
2025-09-25 23:00:49 +0300

    -

--- task_1.py
+++ task_1.py
@@ -7,3 +7,9 @@ for word in words:
         word_count[word_lower] += 1
     else:
         word_count[word_lower] = 1
+unique_words_count = len(word_count)
+print("\n1. Word frequency dictionary:")
+for word, count in word_count.items():
+    print(f"'{word}': {count}")
+
+print(f"\n2. Number of unique words: {unique_words_count}")
