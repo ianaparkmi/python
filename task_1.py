9a59281a33f359cf1163ebb66926feb43476fa53
ianaparkmi <ianaparkmi@gmail.com>
2025-09-27 23:39:21 +0300

    Update task_1.py
    change coding style

--- task_1.py
+++ task_1.py
@@ -1,5 +1,5 @@
-surname=input("Введите фамилию:")
-name=input("Введите имя:")
-patronymic=input("Введите отчество:")
-result=surname+" "+name[0]+"."+patronymic[0]+"."
-print("ФИО:",result)
+surname = input("Введите фамилию:")
+name = input("Введите имя:")
+patronymic = input("Введите отчество:")
+result = f"{surname} {name[0]}.{patronymic[0]}."
+print ("ФИО:",result)
