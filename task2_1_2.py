de66bdb650783bd01b21cab860f14a7617a59503
ianaparkmi <ianaparkmi@gmail.com>
2025-11-04 21:11:09 +0300

    добавила проверку типов

--- task2_1_2.py
+++ task2_1_2.py
@@ -1,45 +1,61 @@
-numbers_input=input('enter a number and use a spase:').split()
-numbers=[]
-float_num=[]
+numbers_input = input('Enter numbers separated by space: ').split()
+numbers = []
+float_num = []
+
 for num_str in numbers_input:
     if '.' in num_str:
-        numbers.append(float(num_str))
-        float_num.append(num_str)
+        num = float(num_str)
+        numbers.append(num)
+        float_num.append(num)
     else:
-        numbers.append(int(num_str))
-max=numbers[0]
-min=numbers[0]
-unique_numbers = []
-again_num=[]
-even=[]
-odd=[]
-negative_num=[]
-sum_5=0
-for num in numbers:
-    if num not in numbers:
-        unique_numbers.append(num)
-    else:
-          if num not in again_num: 
-            again_num.append(num)
-    if num % 2==0:
-        even.append(num)
-    else:
-        odd.append(num)
-    if num<0:
-        negative_num.append(num)
-    if num % 5==0:
-        sum_5 +=num
-    if num>max:
-        max=num
-    if num<min:
-        min=num
+        num = int(num_str)
+        numbers.append(num)
 
-print("unique numbers:", unique_numbers)
-print(" repeating numbers:", again_num)
-print('even numbers:', even)
-print('odd numbers:', odd)
-print('negative numbers:', negative_num)
-print('float numbers:', float_num)
-print('Sum of multiples of 5:',sum_5)
-print("max number: ", max)
-print("min number: ", min)
+if not numbers:
+    print("No numbers entered!")
+    exit()
+
+max_num = numbers[0]
+min_num = numbers[0]
+unique_numbers = []
+again_num = []
+even = []
+odd = []
+negative_num = []
+sum_5 = 0
+
+for num in numbers:
+    if numbers.count(num) == 1:
+        if num not in unique_numbers:
+            unique_numbers.append(num)
+    else:
+        if num not in again_num:
+            again_num.append(num)
+    
+    if isinstance(num, int):
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odd.append(num)
+    
+    if num < 0:
+        negative_num.append(num)
+    
+    if isinstance(num, int) and num % 5 == 0:
+        sum_5 += num
+    
+    if num > max_num:
+        max_num = num
+    if num < min_num:
+        min_num = num
+
+print("\n=== ANALYSIS RESULTS ===")
+print("1. Unique numbers:", unique_numbers)
+print("2. Repeating numbers:", again_num)
+print("3. Even numbers:", even)
+print("   Odd numbers:", odd)
+print("4. Negative numbers:", negative_num)
+print("5. Float numbers:", float_num)
+print("6. Sum of multiples of 5:", sum_5)
+print("7. Max number:", max_num)
+print("8. Min number:", min_num)
