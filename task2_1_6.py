bd6fd4421694f1eb8e708d97fb58afe469ef3fac
ianaparkmi <ianaparkmi@gmail.com>
2025-11-05 09:57:30 +0300

    delit list 'seen'

--- task2_1_6.py
+++ task2_1_6.py
@@ -1,8 +1,6 @@
 user_list=input("enter elements of list and use a space").split()
 unique_list = []
-seen = []
 for elem in user_list:
-    if elem  not in seen:
+    if elem  not in unique_list:
         unique_list.append(elem)
-        seen.append(elem)
 print("list without doublons:", unique_list)
